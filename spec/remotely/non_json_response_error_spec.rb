937ddfdc9626b4813324db44c661824f0b512734
Craig McNamara <craig@caring.com>
2014-08-08 00:07:29 -0700

    Use the proper rspec style.

--- spec/remotely/non_json_response_error_spec.rb
+++ spec/remotely/non_json_response_error_spec.rb
@@ -4,6 +4,6 @@ describe Remotely::NonJsonResponseError do
   let(:exception) { Remotely::NonJsonResponseError.new("<html lang='en'><head><title>Not JSON</title></head></html>") }
 
   it "should include the HTML response in the exception message" do
-    expect(exception.message).to match(/<title>Not JSON<\/title>/)
+    exception.message.should match(/<title>Not JSON<\/title>/)
   end
 end
